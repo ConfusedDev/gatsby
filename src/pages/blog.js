bbccacbef67a7e32a4036b269d4532234a6eadda
ConfusedDev <54333889+ConfusedDev@users.noreply.github.com>
2022-03-15 17:16:07 -0400

    Update blog page to render mdx files from the data layer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,30 +1,37 @@
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))}
-      </ul>
+      {
+        data.allMdx.nodes.map((node)=>(
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>{node.body}</MDXRenderer>
+          </article>
+        ))
+      }
     </Layout>
   );
 };
 
 export const query = graphql`
 query {
-  allFile {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
     nodes {
-      name
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+      }
+      id
+      body
     }
   }
 }
-
 `
 
 export default BlogPage;
