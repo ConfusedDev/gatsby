354116811d7d5fe0968f59ab86c388064509819b
ConfusedDev <54333889+ConfusedDev@users.noreply.github.com>
2022-03-15 19:52:06 -0400

    update component tree render mdx content

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Layout from "../components/layout";
-import { graphql } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from "../../components/layout";
+import { graphql, Link } from "gatsby";
 
 const BlogPage = ({ data }) => {
   return (
@@ -9,9 +8,8 @@ const BlogPage = ({ data }) => {
       {
         data.allMdx.nodes.map((node)=>(
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2><Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link></h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>{node.body}</MDXRenderer>
           </article>
         ))
       }
@@ -28,7 +26,7 @@ query {
         title
       }
       id
-      body
+      slug
     }
   }
 }
