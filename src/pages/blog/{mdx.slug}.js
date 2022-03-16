ffb2ee95de39b8698f91bd6c571479987348d410
ConfusedDev <54333889+ConfusedDev@users.noreply.github.com>
2022-03-15 21:07:35 -0400

    add image rendering to blog posts

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,11 +2,15 @@ import React from "react";
 import Layout from "../../components/layout";
 import {graphql} from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const BlogPost = ({data}) => {
+
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt}/>
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -16,10 +20,18 @@ const BlogPost = ({data}) => {
 
 export const query = graphql`
 query ($id: String) {
-  mdx(id: {eq: $id}) {
+  mdx(id: {eq: $id}, frontmatter: {}) {
     frontmatter {
       title
       date(formatString: "MMMM D, YYYY")
+      hero_image_alt
+      hero_image_credit_link
+      hero_image_credit_text
+      hero_image{
+				childImageSharp{
+					gatsbyImageData
+        }
+      }
     }
     body
   }
